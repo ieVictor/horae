86ad67685494c4ff4bae1bb47a9a6d9914cb61b6
ieVictor <victor_tiss@outlook.com>
2026-05-07 19:43:03 -0300

    style: use direct import

--- src/components/input.rs
+++ src/components/input.rs
@@ -1,10 +1,10 @@
 use crossterm::event::KeyCode;
-use tui_input::Input;
+use tui_input::{Input, InputRequest};
 
 pub fn apply_input(input: &mut Input, code: KeyCode) {
     match code {
-        KeyCode::Char(c) => { input.handle(tui_input::InputRequest::InsertChar(c)); }
-        KeyCode::Backspace => { input.handle(tui_input::InputRequest::DeletePrevChar); }
+        KeyCode::Char(c) => { input.handle(InputRequest::InsertChar(c)); }
+        KeyCode::Backspace => { input.handle(InputRequest::DeletePrevChar); }
         _ => {}
     }
 }
